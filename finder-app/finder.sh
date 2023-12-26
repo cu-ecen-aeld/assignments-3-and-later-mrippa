df122946ef324b8e7498ad63f624ddcd9fa94864
Matt Rippa <matt.rippa@noirlab.edu>
2023-12-26 00:19:58 -1000

    Implement finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+filesdir=$1
+searchstr=$2
+
+if [ $# -eq 0 ]
+then
+    echo "Error. Usage: finder.sh <filesdir> <searchstring> "
+    exit 1
+elif [ ! -d $filesdir ] 
+then 
+    echo "Error: Direcrory does not exist."
+    exit 1
+else 
+
+    X=$( find `readlink -f $filesdir` -type f | wc -l )
+    Y=$( grep -r $searchstr $filesdir | wc -l )
+    echo "The number of files are $X and the number of matching lines are $Y"
+    exit 0
+fi
