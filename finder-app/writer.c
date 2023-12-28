2cfd14ebc71c06a02ff5f471a2b9c5d2b3bce234
Matt Rippa <matt.rippa@noirlab.edu>
2023-12-27 14:10:34 -1000

    Implement assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <syslog.h>
+
+#define MY_STRING_LIMIT 40
+FILE *writefile;
+
+/**
+ * @brief
+ *
+ * This partially generated using ChatGPT at https://chat.openai.com/ with prompts
+ * including prompt:
+ *
+ * " main routine in c to validate an input string as arg 1"
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
+int main(int argc, char *argv[])
+{
+
+    char filepath[MY_STRING_LIMIT];
+    char writestring[MY_STRING_LIMIT];
+    int size =0;
+
+    if (argc < 3)
+    {
+        printf("Usage: %s <writefile> <writestring>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    openlog("myprogram", LOG_PID, LOG_LOCAL0);
+
+    strncpy(filepath, argv[1], MY_STRING_LIMIT);
+    strncpy(writestring, argv[2], MY_STRING_LIMIT);
+    printf("arg1: %s\n", filepath);
+    printf("arg2: %s\n", writestring);
+    
+    writefile = fopen(filepath, "w");
+    if (writefile == NULL) {
+        syslog(LOG_ERR, "Error opening %s\n", filepath);
+        perror("fopen step");
+        exit(1);
+    }
+
+    size = fprintf(writefile, "%s", writestring);
+
+    if (size <= 0) {
+        syslog(LOG_ERR, "Error writing to file %s\n", filepath);
+        perror("writing step");
+        exit(1);
+    }
+
+    fclose(writefile);
+
+    syslog(LOG_INFO, "Success!\n");
+    exit(0); 
+
+
+}
+
